fc9d8f5445705b347236e179b049b68a872a4dbd
Rei Zhang <mauoser@outlook.com>
2025-11-04 01:43:08 -0500

    Firebase configurations and user data saved successfully using handlers

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import AuthHandler from "@/handlers/auth-handler";
 import { Outlet } from "react-router";
 
 export const PublicLayout = () => {
   return (
     <div className="w-full">
+      <AuthHandler></AuthHandler>
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
